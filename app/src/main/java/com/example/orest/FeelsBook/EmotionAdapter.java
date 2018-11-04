c7e52ddf186fbdedaed1aaae2543d7af3f0fded7
Skryt <cokan@ualberta.ca>
2018-11-04 11:50:56 -0700

    Update EmotionAdapter.java

--- app/src/main/java/com/example/orest/FeelsBook/EmotionAdapter.java
+++ app/src/main/java/com/example/orest/FeelsBook/EmotionAdapter.java
@@ -1,23 +1,4 @@
 /**
- *   Displays the number of unique feeling types stored in the application
- *
- *     Copyright (C) 2018 Tem Tamre
- *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/**
  * @authors Skryt
  *
  * EmotionAdapter is an adapter class that simply takes data and places it into specified locations
